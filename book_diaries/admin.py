8230a53d745c18e9c1d6085b082b0aa345d660a4
Jordan Onwodi <Jonwodi@hotmail.co.uk>
2022-04-11 16:06:01 +0100

    authentication for registered users working

--- book_diaries/admin.py
+++ book_diaries/admin.py
@@ -2,12 +2,6 @@ from django.contrib import admin
 
 from .models import Book, BookNote
 
+admin.site.register(Book)
 
-@admin.register(Book)
-class Book(admin.ModelAdmin):
-    pass
-
-
-@admin.register(BookNote)
-class BookNote(admin.ModelAdmin):
-    pass
+admin.site.register(BookNote)
